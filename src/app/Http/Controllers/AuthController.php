dd258ccb9228a58450cce11b8ac39401062bdfb1
vipin <vchand@taxbackinternational.com>
2023-08-22 00:51:24 +0530

    first commeit to test github action

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -9,24 +9,36 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        if (!$token = auth()->attempt(['email' => $request->email, 'password' => $request->password])){
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
         return response()->json([
-            'messae' => 'hellow world'
+           'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => 60,
+//            'refresh_token' => auth()->refresh(),
         ]);
-//        if (!$token = auth()->attempt(['email' => $request->email, 'password' => $request->password])){
-//            return response()->json(['error' => 'Unauthorized'], 401);
-//        }
-//
-//        return response()->json([
-//           'access_token' => $token,
-//            'token_type' => 'bearer',
-//            'expires_in' => 60,
-//        ]);
 
     }
 
     public function me()
     {
         return 'only login user can access this page';
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
     }
 
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -22,16 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('login', [AuthController::class, 'login']);
 
-Route::get('login', function (){
-    return 'this is login page';
+Route::get('login', static function (){
+    return response()->json(['message' => 'Please login']);
 })->name('login');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('me', [AuthController::class, 'me']);
 
 //    Route::post('logout', 'AuthController@logout');
-//    Route::post('refresh', 'AuthController@refresh');
+    Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('/projects', [ProjectController::class, 'createProject']);
-});
     Route::get('/projects', [ProjectController::class, 'index']);
+});
 
